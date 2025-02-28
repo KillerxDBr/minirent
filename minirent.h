8c623070b6aa35a1f51065a812d54433eb2410df
KillerxDBr <97485370+KillerxDBr@users.noreply.github.com>
2025-02-28 17:35:41 -0400

    fix for UNICODE macros

--- minirent.h
+++ minirent.h
@@ -40,8 +40,11 @@
 #ifndef MINIRENT_H_
 #define MINIRENT_H_
 
+#include <assert.h>
+#include <stdio.h>
+
 #define WIN32_LEAN_AND_MEAN
-#include "windows.h"
+#include <windows.h>
 
 struct dirent
 {
@@ -61,7 +64,7 @@ int closedir(DIR *dirp);
 struct DIR
 {
     HANDLE hFind;
-    WIN32_FIND_DATA data;
+    WIN32_FIND_DATAA data;
     struct dirent *dirent;
 };
 
@@ -69,12 +72,12 @@ DIR *opendir(const char *dirpath)
 {
     assert(dirpath);
 
-    char buffer[MAX_PATH];
+    char buffer[MAX_PATH+1];
     snprintf(buffer, MAX_PATH, "%s\\*", dirpath);
 
     DIR *dir = (DIR*)calloc(1, sizeof(DIR));
 
-    dir->hFind = FindFirstFile(buffer, &dir->data);
+    dir->hFind = FindFirstFileA(buffer, &dir->data);
     if (dir->hFind == INVALID_HANDLE_VALUE) {
         // TODO: opendir should set errno accordingly on FindFirstFile fail
         // https://docs.microsoft.com/en-us/windows/win32/api/errhandlingapi/nf-errhandlingapi-getlasterror
@@ -99,7 +102,7 @@ struct dirent *readdir(DIR *dirp)
     if (dirp->dirent == NULL) {
         dirp->dirent = (struct dirent*)calloc(1, sizeof(struct dirent));
     } else {
-        if(!FindNextFile(dirp->hFind, &dirp->data)) {
+        if(!FindNextFileA(dirp->hFind, &dirp->data)) {
             if (GetLastError() != ERROR_NO_MORE_FILES) {
                 // TODO: readdir should set errno accordingly on FindNextFile fail
                 // https://docs.microsoft.com/en-us/windows/win32/api/errhandlingapi/nf-errhandlingapi-getlasterror
